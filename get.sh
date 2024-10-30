0bddee6660a1bf2339652ed721799025bfd858d8
alifeee <alifeee.web@outlook.com>
2024-10-30 17:05:41 +0000

    initial commit

---
+++ get.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# get events from https://www.welcometosheffield.co.uk/visit/what-s-on/all-events
+#  and attempt to format as json
+
+URL="https://www.welcometosheffield.co.uk/visit/what-s-on/all-events/"
+URL_base="https://www.welcometosheffield.co.uk"
+page="${1}"
+if [ -z "${page}" ]; then
+  echo "use: ./get.sh page"
+  echo "please specify page number (1, 2, 3, ...)"
+  exit 1
+fi
+
+events_html=$(curl -s "${URL}?page=${page}")
+results=$(echo "${events_html}" | sed 's/\$/USD/g' | hxnormalize -l 240 -x | hxselect -s '$' "div.search-result")
+mapfile -d$ a <<< "${results}"; unset 'a[-1]'
+# echo "${#a[@]}" # length
+# echo "${a[@]}" # items
+
+all_json='['
+sep=''
+
+for event in "${a[@]}"; do
+  # name
+  name=$(echo "${event}" | hxselect -c "div h3 a")
+  # event link
+  link=$(echo "${event}" | hxselect -c "div h3 a::attr(href)")
+  # description
+  description=$(echo "${event}" | hxselect -c "div p" | sed 's/^ * / /g' | tr -d '\n')
+	# full date/time
+  datetime=$(echo "${event}" | hxselect -c "div.meta small" | sed 's/<[^>]*>//g' | sed 's/^ *//' | sed 's/ *$//')
+  # start date
+  start_date=$(echo "${datetime}" | awk -F' ?- ?' '{print $1}' | sed 's/\./:/g')
+  if [ ! -z "${start_date}" ]; then
+    start_date_unix=$(date --date="${start_date}" +%s)
+  else
+    start_date_unix=""
+  fi
+  # end date
+  end_date=$(echo "${datetime}" | awk -F' ?- ?' '{print $2}' | sed 's/\./:/g')
+  if [[ "${end_date}" =~ [0-9][0-9]:[0-9][0-9] ]]; then
+    maybedate=$(echo "${datetime}" | awk -F' ' '{printf "%s %s %s", $1, $2, $3}')
+    end_date="${maybedate} ${end_date}"
+  fi
+  if [ ! -z "${end_date}" ]; then
+    end_date_unix=$(date --date="${end_date}" +%s)
+  else
+    end_date_unix=""
+  fi
+  # tags
+  tags=$(echo "${event}" | hxselect -s '\n' -c "div.tags small" | sed 's/.*<\/svg> *//g' | sed '/^$/d' | recode html..ascii)
+
+  # create json
+  json=$(jq -n --arg url_base "${URL_base}" \
+  '{
+    "name": $ARGS.positional[0],
+    "link": ($url_base + $ARGS.positional[1]),
+    "description": $ARGS.positional[2],
+		"datetime": $ARGS.positional[3],
+    "start_date": $ARGS.positional[4],
+    "end_date": $ARGS.positional[5],
+    "start_date_unix": $ARGS.positional[6],
+    "end_date_unix": $ARGS.positional[7],
+    "tags": $ARGS.positional[8] | split("\n")
+  }
+  ' \
+  --args "${name}" "${link}" "${description}" "${datetime}" "${start_date}" "${end_date}" "${start_date_unix}" "${end_date_unix}" "${tags}")
+  all_json="${all_json}""${sep}""${json}"
+  sep=','
+done
+
+all_json="${all_json}]"
+
+echo "${all_json}" | jq
---
+++ get_all.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# get all events from https://www.welcometosheffield.co.uk/visit/what-s-on/all-events/
+#  by finding total pages and then calling ./get.sh for each page
+# saves the output to events.json
+
+echo -e './get_all.sh pages sleep_time'
+echo '    pages: total pages to index (default -1 / all)'
+echo '    sleep_time: time to sleep between pages (default 1)'
+sleep 2
+
+stop_after="${1:--1}"
+sleep_time="${2:-1}"
+save_to="events.json"
+
+echo "getting all events..." > /dev/stderr
+events_html=$(curl -s "https://www.welcometosheffield.co.uk/visit/what-s-on/all-events/?page=1")
+last_page_num=$(echo "${events_html}" | hxnormalize -l 240 -x | hxselect -c "a[data-pagenumber]:nth-last-child(2) span")
+
+echo "getting a total of ${last_page_num} pages..." > /dev/stderr
+
+mega_json="[]"
+
+for page in `seq 1 "${last_page_num}"`; do
+  if [ ! "${stop_after}" == -1 ] && [ "${stop_after}" -lt "${page}" ]; then
+    echo "told to stop after ${stop_after} pages... stopping..." > /dev/stderr
+    break
+  fi
+
+  echo "getting page ${page}..."
+  json_arr=$(./get.sh "${page}")
+
+  items=$(echo "${json_arr}" | jq '.|length')
+  echo "  ${items} events found on page ${page}"
+
+  mega_json=$(echo "${mega_json}" | jq --argjson next "${json_arr}" '[.[], $next[]]')
+
+  items=$(echo "${mega_json}" | jq '.|length')
+  echo "  total events so far: ${items}"
+  if [ "${sleep_time}" -gt 0 ]; then
+    echo "  sleeping ${sleep_time} secs..."
+    sleep "${sleep_time}"
+  fi
+done
+
+echo "saving to ${save_to}" > /dev/stderr
+last_edited_unix=$(date -u +%s)
+last_edited=$(date -u -d "@${last_edited_unix}" "+%c")
+echo "  ...with timestamp ${last_edited} / ${last_edited_unix}" > /dev/stderr
+echo "${mega_json}" | jq -c '{
+  "last_edited": $ARGS.positional[0],
+  "last_edited_unix": $ARGS.positional[1],
+  "events": .
+}' --args "${last_edited}" "${last_edited_unix}" > "${save_to}"
+echo "saved!" > /dev/stderr
