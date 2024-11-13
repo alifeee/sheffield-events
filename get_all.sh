a0a741f55d7331fb22d201195727e2a9c8e4ea52
alifeee <alifeee.web@outlook.com>
2024-11-13 20:23:53 +0000

    improve logging

--- get_all.sh
+++ get_all.sh
@@ -3,6 +3,8 @@
 #  by finding total pages and then calling ./get.sh for each page
 # saves the output to events.json
 
+date >> /dev/stderr
+date
 echo  './get_all.sh pages sleep_time'
 echo '    pages: total pages to index (default -1 / all)'
 echo '    sleep_time: time to sleep between pages (default 1)'
@@ -11,20 +13,17 @@ stop_after="${1:--1}"
 sleep_time="${2:-1}"
 save_to="events.json"
 
-echo "backing up existing file to ${save_to}.bak"
-cp "${save_to}" "${save_to}.bak"
-
-echo "getting all events..." > /dev/stderr
+echo "getting all events..."
 events_html=$(curl -s "https://www.welcometosheffield.co.uk/visit/what-s-on/all-events/?page=1")
 last_page_num=$(echo "${events_html}" | hxnormalize -l 240 -x | hxselect -c "a[data-pagenumber]:nth-last-child(2) span")
 
-echo "getting a total of ${last_page_num} pages..." > /dev/stderr
+echo "getting a total of ${last_page_num} pages..."
 
 mega_json="[]"
 
 for page in `seq 1 "${last_page_num}"`; do
   if [ ! "${stop_after}" == -1 ] && [ "${stop_after}" -lt "${page}" ]; then
-    echo "told to stop after ${stop_after} pages... stopping..." > /dev/stderr
+    echo "told to stop after ${stop_after} pages... stopping..."
     break
   fi
 
@@ -44,13 +43,22 @@ for page in `seq 1 "${last_page_num}"`; do
   fi
 done
 
-echo "saving to ${save_to}" > /dev/stderr
+# backup existing copy
+date_str=$(date "+%Y-%m-%dT%H%M%S")
+backup_fname="backups/${date_str}_${save_to}"
+echo "backing up existing file to ${backup_fname}"
+cp "${save_to}" "${backup_fname}"
+gzip "${backup_fname}"
+
+# save
+echo "saving to ${save_to}"
 last_edited_unix=$(date -u +%s)
 last_edited=$(date -u -d "@${last_edited_unix}" "+%c")
-echo "  ...with timestamp ${last_edited} / ${last_edited_unix}" > /dev/stderr
+echo "  ...with timestamp ${last_edited} / ${last_edited_unix}"
 echo "${mega_json}" | jq -c '{
   "last_edited": $ARGS.positional[0],
   "last_edited_unix": $ARGS.positional[1],
   "events": .
 }' --args "${last_edited}" "${last_edited_unix}" > "${save_to}"
-echo "saved!" > /dev/stderr
+echo "saved!"
+echo ""
